ba96b50d165133a7342a4203d91597665079f7d5
truongqh <truongnqt02@gmail.com>
2024-02-21 23:31:44 +0700

    Add project files.

---
+++ AssignmentDay5/Book.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Messaging;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AssignmentDay5
+{
+	 class Book
+	{
+		private string _isbn;
+		private string _bookName;
+		private string _authorName;
+		private string _publisherName;
+
+		// Constructor
+		public Book() { }
+		public Book(string isbn, string bookName, string authorName, string publisherName)
+		{
+			this._isbn = isbn;
+			this._bookName = bookName;
+			this._authorName = authorName;
+			this._publisherName = publisherName;
+			
+		}
+		public void SetIsbn(string isbn)
+		{
+			try
+			{
+				long parsedIsbn = Convert.ToInt64(isbn);
+				this._isbn = isbn;
+			}
+			catch (FormatException)
+			{
+				Console.WriteLine("Invalid input for ISBN. Please enter a valid number.");
+			}
+			
+			this._isbn = isbn; 
+		}
+		public string GetIsbn() { return this._isbn; }
+		public void SetBookName(string bookName){ this._bookName = bookName; 		}	
+		public string GetBookName() { return this._bookName; }
+		public void SetAuthorName(string authorName) { this._authorName = authorName; }
+		public string GetAuthorName() { return this._authorName; }
+		public void SetPublisherName(string publisherName) { this._publisherName = publisherName; }
+		public string GetPublisherName() { return this._publisherName; }
+		
+		public string GetBookInformation()
+		{
+			return $"{_isbn} {_bookName} {_authorName} {_publisherName}";
+		}
+	}
+}
---
+++ AssignmentDay5/Program.cs
@@ -0,0 +1,113 @@
+﻿using Microsoft.SqlServer.Server;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace AssignmentDay5
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			LinkedList<Book> books = new LinkedList<Book>();
+			Menu(books);
+		}
+		public static bool CheckNumber(string number)
+		{
+			string regexPattern = @"[0-9]{9}";
+
+
+			if (Regex.IsMatch(number, regexPattern))
+			{
+				return true; 
+			}
+			else
+			{
+				return false;
+			}
+		}
+		public static void  AddBook(LinkedList<Book> books)
+		{ 
+
+            Console.WriteLine("ISBN: ");
+			string isbn = Console.ReadLine();
+			while(!CheckNumber(isbn))
+			{
+				Console.WriteLine("Invalid input for ISBN. Please enter a valid number(9 character). Input again: ");
+				isbn = Console.ReadLine();
+				
+			}
+			Console.Write("Book Name: ");
+			string bookName=Console.ReadLine();
+			Console.Write("Author Book: ");
+			string authorName = Console.ReadLine();
+			Console.Write("Publisher Name: ");
+			String publisherName=Console.ReadLine();
+			try
+			{
+				Book book = new Book(isbn, bookName, authorName, publisherName);
+				books.AddLast(book);
+			}
+			catch(Exception ex)
+			{
+				Console.WriteLine(ex.Message);
+			}
+        }
+		public static void DisplayallBooks(LinkedList<Book> books)
+		{
+            Console.WriteLine("Detail list books");
+            foreach(var item in books)
+			{
+				Console.WriteLine(item.GetBookInformation());
+			}
+        }
+		public static void Menu(LinkedList<Book> books)
+		{
+			while(true)
+			{
+				Console.WriteLine("Menu");
+				Console.WriteLine("1. Add book");
+				Console.WriteLine("2. Display books");
+				Console.WriteLine("3  Exit");
+				string choice;
+                Console.Write("Choice : ");
+                choice = Console.ReadLine();
+				try
+				{
+					if (int.TryParse(choice, out int nchoice))
+					{
+						switch (nchoice)
+						{
+							case 1:
+								AddBook(books);
+								break;
+							case 2:
+								DisplayallBooks(books);
+								break;
+							case 3:
+								Environment.Exit(0);
+								break;
+							default: 
+								Console.WriteLine("No option");
+								break;
+						}
+					}
+					else
+					{
+						Console.WriteLine("Invalid input. Please enter a number.");
+					}
+				}
+
+				catch (Exception ex)
+				{
+					Console.WriteLine($"Error: {ex.Message}");
+				}
+			}
+		}
+	}
+}
